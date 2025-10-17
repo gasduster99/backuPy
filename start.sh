7771cc03bcd1e5541de3be3ac1dbed416a133fe4
chisquareotops <nick.grunloh@noaa.gov>
2025-10-17 12:59:32 -0700

    updating start/stop.sh

--- start.sh
+++ start.sh
@@ -6,12 +6,12 @@ export DISPLAY=:1
 #foundDisplay=$(ls -t /private/tmp/com.apple.launchd.*/org.xquartz:0 | head -1)
 #export DISPLAY=$foundDisplay
 
-#
-nohup vlc -I dummy -q ~/Documents/programs/backupSys/upTone.mp3 &
+nohup vlc -I dummy -q ~/Documents/programs/backuPy/upTone.mp3 &
+#nohup vlc -I dummy -q ~/Documents/programs/backupSys/upTone.mp3 &
 #afplay ~/Documents/programs/backupSys/upTone.mp3
 
-#
-xcowsay --image ~/Documents/programs/backupSys/tux.png -t 10 --bubble-at -150,-100 --think Starting Backup
+xcowsay --image ~/Documents/programs/backuPy/tux.png -t 10 --bubble-at -150,-100 --think Starting Backup
+#xcowsay --image ~/Documents/programs/backupSys/tux.png -t 10 --bubble-at -150,-100 --think Starting Backup
 
 
 
--- stop.sh
+++ stop.sh
@@ -6,13 +6,13 @@ export DISPLAY=:1
 #foundDisplay=$(ls -t /private/tmp/com.apple.launchd.*/org.xquartz:0 | head -1)
 #export DISPLAY=$foundDisplay
 
-#
-nohup vlc -I dummy -q ~/Documents/programs/backupSys/downTone.mp3 &
+nohup vlc -I dummy -q ~/Documents/programs/backuPy/downTone.mp3 &
+#nohup vlc -I dummy -q ~/Documents/programs/backupSys/downTone.mp3 &
 #vlc -I dummy ~/Documents/programs/backupSys/downTone.mp3 &
 #afplay ~/Documents/programs/backupSys/downTone.mp3
 
-#
-xcowsay --image ~/Documents/programs/backupSys/tux.png -t 10 --bubble-at -150,-100 --think Backup Complete
+xcowsay --image ~/Documents/programs/backuPy/tux.png -t 10 --bubble-at -150,-100 --think Backup Complete
+#xcowsay --image ~/Documents/programs/backupSys/tux.png -t 10 --bubble-at -150,-100 --think Backup Complete
 
 
 
