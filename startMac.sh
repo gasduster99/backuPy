cdf5eb4629b400354a2477f72e52e77fd6d4e5bb
gasduster99 <grunloh@soe.ucsc.edu>
2025-10-17 19:11:27 -0700

    adding template start/stop for Mac and linux

--- startMac.sh
+++ startMac.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+##Linux
 #export DISPLAY=:1 
-#export DISPLAY=/private/tmp/com.apple.launchd.KAxnDWCQtJ/org.xquartz:0
+#Mac
 foundDisplay=$(ls -t /private/tmp/com.apple.launchd.*/org.xquartz:0 | head -1)
 export DISPLAY=$foundDisplay
 
-#
-afplay ~/Documents/programs/backuPy/upTone.mp3
+##Linux
+#nohup vlc -I dummy -q ~/Documents/programs/backuPy/upTone.mp3 &
+#Mac
+afplay ~/Documents/programs/backuPy/upTone.mp3 &
 
 #
 xcowsay --image ~/Documents/programs/backuPy/tux.png -t 10 --bubble-at -150,-100 --think Starting Backup
--- stopMac.sh
+++ stopMac.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+##Linux
 #export DISPLAY=:1 
-#export DISPLAY=/private/tmp/com.apple.launchd.KAxnDWCQtJ/org.xquartz:0
+#Mac
 foundDisplay=$(ls -t /private/tmp/com.apple.launchd.*/org.xquartz:0 | head -1)
 export DISPLAY=$foundDisplay
 
-#
+##Linux
+#nohup vlc -I dummy -q ~/Documents/programs/backuPy/downTone.mp3 &
+#Mac
 afplay ~/Documents/programs/backuPy/downTone.mp3
 
 #
